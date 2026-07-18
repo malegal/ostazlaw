f31c5e6387f4c54edbfdc49fffc93cc2094ec584
malegal <ma.legal.eg@outlook.com>
2026-07-19 01:55:05 +0300

    Add SkeletonCard component for loading state

---
+++ components/SkeletonCard.js
@@ -0,0 +1,10 @@
+export default function SkeletonCard() {
+  return (
+    <div className="skeleton-card">
+      <div className="skeleton-image"></div>
+      <div className="skeleton-title"></div>
+      <div className="skeleton-meta"></div>
+      <div className="skeleton-excerpt"></div>
+    </div>
+  );
+}
